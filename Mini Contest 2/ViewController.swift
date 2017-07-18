102283c52ef997b82bae6bc8cb8342e10be6da74
MisterNay <misternay@misternays-macbook-pro.local>
2017-07-18 09:59:05 +0700

    Initial Commit

---
+++ Mini Contest 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Contest 2
+//
+//  Created by MisterNay on 7/18/2560 BE.
+//  Copyright © 2560 MisterNay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
